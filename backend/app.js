6a8e2856aa796d4f12acc9ecb36d8ce5358870bd
Uznain Rashid <147235060+uznainrashid@users.noreply.github.com>
2026-03-19 02:48:42 +0500

    changing done

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/auth", AuthRouter);
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Complete");
 });
 app.use("/products", ProductRouters);
