60198053e7307dd739e878a13b37a3da9923c450
Uznain Rashid <147235060+uznainrashid@users.noreply.github.com>
2026-03-19 03:47:55 +0500

    changing done

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ const ProductRouters = require("./routes/ProductRouter");
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin:"https://mern-app-nine-iota.vercel.app"
+  origin:"https://mern-app-nine-iota.vercel.app" 
 })); 
 app.use(bodyParser.json());
 
-app.use("/auth", AuthRouter);
+app.use("/auth", AuthRouter );
 
 app.use("/products", ProductRouters);
 app.get("/", (req, res) => {
